3ddc8fbf0a1ccfb4ef70746e7b897af7b86d43a7
Amanda <amanda525911@gmail.com>
2022-10-25 11:46:11 -0300

    Adicionado Treino

---
+++ treino.py
@@ -0,0 +1,9 @@
+n1= float(input("digite o primeiro número: "))
+n2= float(input("digite o segundo número: "))
+
+print("soma = ", n1 + n2)
+print("subtração = ", n1 - n2)
+print("multiplicação = ", n1 * n2)
+print("divisão = ", n1 / n2)
+print("quadrado de 1 = ", n1**2)
+print("quadrado de 2 = ", n2**2)
