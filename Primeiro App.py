06a873c92a154314f6b2a55a24110025b5419e44
Amanda <amanda525911@gmail.com>
2022-10-25 11:19:46 -0300

    Criado APP IMC
    Iniciado a entrada e saída de dados do aplicativo

---
+++ Primeiro App.py
@@ -0,0 +1,12 @@
+# coletando dados do usuario
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade: "))
+altura = float(input("digite sua altura: "))
+peso = float(input("digite sua peso: "))
+
+print("-" * 30)
+print("Os Dados coletados foram:")
+print("nome: ", nome)
+print("idade: ", idade, " anos")
+print("altura: ", altura)
+print("peso: ", peso," quilos")
