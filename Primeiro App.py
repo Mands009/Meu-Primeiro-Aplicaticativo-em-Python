4483a1cc346d66f7117743e8d476add037753964
Amanda <amanda525911@gmail.com>
2022-10-26 11:49:43 -0300

    Update App IMC

--- Primeiro App.py
+++ Primeiro App.py
@@ -12,3 +12,18 @@ print("idade: ", idade, " anos")
 print("altura: ", altura)
 print("peso: ", peso," quilos")
 print("IMC: ", IMC)
+
+if IMC < 16:
+    print("Magreza Grave! Cuidado!!")
+elif IMC < 18.5:
+    print("Magreza leve! Abaixo do peso normal.")
+elif IMC < 24.9:
+    print("Peso normal. Parabéns!")
+elif IMC < 29.9:
+    print("Excesso de peso. Cuidado!")
+elif IMC < 34.9:
+    print("Obesidade classe I. Cuidado em!")
+elif IMC < 39.9:
+    print("Obesidade classe II. Tá só piorando!!")
+else:
+    print("Obesidade classe III. Meu Deusss!!!!!!")
